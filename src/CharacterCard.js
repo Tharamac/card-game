88eb5bcad68288beae43a698afba76a83d058158
Macxiiz <tharathorn.wang@gmail.com>
2019-09-19 10:13:56 +0700

    set onClick state on or off

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,9 +1,28 @@
 import React,{Component} from 'react';
 
 export default class CharacterCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            active: false,
+        }
+    }
+
+    activate = () => {
+        if(!this.state.active){
+            this.setState({active : true})
+        }else{
+            this.setState({active : false})
+        }
+    }
+
     render(){
-        return(
-            <div className="card">{this.props.value}</div>
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+       return(
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
         );
     }
 }
+
