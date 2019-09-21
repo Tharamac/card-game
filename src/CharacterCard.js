4741bea57360d02c6d0b0245733df07b706f9e55
Macxiiz <tharathorn.wang@gmail.com>
2019-09-21 14:01:26 +0700

    Add new game button

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -14,6 +14,7 @@ export default class CharacterCard extends Component {
         if(prevProps.attempt != this.props.attempt){
             this.setState({active :false})
         }
+        
     }
 
     activate = () => {
